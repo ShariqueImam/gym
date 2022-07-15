5dbf9c0a7ca5df08ba955aa3bd37034794ca015f
Sharique Imam <shariquegithubweb@gmail.com>
2022-07-15 17:16:55 +0500

    asdf

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 const style = {
   wrapper:
-    "h-[10vh] w-[100%] md:w-[95%] lg:w-[90%] flex mx-auto items-center jusitfy-center",
+    "h-[10vh] w-[100%] md:w-[95%] lg:w-[90%] flex mx-auto items-center jusitfy-center px-3",
   logo: "text-orange-500 text-2xl flex-1",
   container:'flex items-center justify-center'
 };
