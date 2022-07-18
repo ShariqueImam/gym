00e4434253aad86e7fe08e9bd113d085fe56ba49
Sharique Imam <shariquegithubweb@gmail.com>
2022-07-18 15:08:12 +0500

    adsf

--- components/FinalPage/MoneyBack.js
+++ components/FinalPage/MoneyBack.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Image from "next/image";
+import { GiConfirmed } from "react-icons/gi";
 const style = {
   wrapper:
-    "my-12 bg-neutral-900 border-2 border-stone-400 px-12 md:px-24 py-6 md:py-16 flex flex-col md:flex-row w-[95%] md:w-[60%] lg:w-[50%] mx-auto",
+    "my-12 bg-neutral-900 border-2 border-stone-400 px-12 md:px-24 py-6 md:py-16 flex flex-col md:flex-row w-[95%] md:w-[60%] lg:w-[50%] mx-auto items-center",
   container: "flex flex-col my-6",
   heading: "text-gray-200 text-3xl md:text-4xl my-8",
   para: "text-gray-400 mr-12",
@@ -20,7 +21,7 @@ const MoneyBack = () => {
           applicable limitations in our money-back policy
         </p>
       </div>
-      <Image src={'/final/download.webp'} height={500} width={700}/>
+      <GiConfirmed className="text-5xl md:text-9xl text-orange-600 scale-[3] md:scale-[4] lg:scale-[3.2] my-12 md:my-0"/>
     </div>
   );
 };
